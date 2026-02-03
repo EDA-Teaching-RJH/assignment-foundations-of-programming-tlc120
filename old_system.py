c5bc507eb1a3ff8706c9f315ccc6c7255d36c37c
tlc120 <tlc1@kent.ac.uk>
2026-02-03 14:13:46 +0000

    5.  In line 59, the code `print("High ranking officers: " + count)`causes a TypeError because the `count` is an integer that cannot be added to a string. It should be `print("High ranking officers: " ,  count)`.

--- old_system.py
+++ old_system.py
@@ -56,7 +56,7 @@ def run_system_monolith():
             for rank in r:
                 if rank == "Captain" or "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            print("High ranking officers: " , count) 
             
         elif opt == "5":
             print("Shutting down.")
