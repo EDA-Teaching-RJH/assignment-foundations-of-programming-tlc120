d2cce29dca7c4b12d2e50a59c84ec4b9c61567de
tlc120 <tlc1@kent.ac.uk>
2026-02-04 12:34:22 +0000

    Add feature 1. `init_database()`

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,4 +1,8 @@
-Names = ["James", "William", "Worf", "Hikaru", "Data"]
-Ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
-Divisions = ["Command", "Command", "Security", "Command", "Operations"]
-IDs = ["100", "101", "102", "103", "104"]
+def main():
+    def init_database():
+        Names = ["James", "William", "Worf", "Hikaru", "Data"]
+        Ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
+        Divisions = ["Command", "Command", "Security", "Command", "Operations"]
+        IDs = ["100", "101", "102", "103", "104"]
+        
+        return Names, Ranks, Divisions, IDs
