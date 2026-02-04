ab8420086e01b56dfefb363f2f851d3f14b9d65b
tlc120 <tlc1@kent.ac.uk>
2026-02-04 12:24:12 +0000

    Part B: Add `Names`, `Ranks`, `Divisions`, and `IDs`

---
+++ fleet_manager.py
@@ -0,0 +1,4 @@
+Names = ["James", "William", "Worf", "Hikaru", "Data"]
+Ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
+Divisions = ["Command", "Command", "Security", "Command", "Operations"]
+IDs = ["100", "101", "102", "103", "104"]
