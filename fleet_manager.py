4e4b26b39abf29879c65e30dde1b49d3981aab1d
tlc120 <tlc1@kent.ac.uk>
2026-02-04 21:21:46 +0000

    Add feature 4. `remove_member(names, ranks, divs, ids)`

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,6 +1,6 @@
 def main():
     def init_database():
-        names = ["James", "William", "Worf", "Hikaru", "Data"]
+        names = ["James T. Kirk", "William Riker", "Worf", "Hikaru Sulu", "Data"]
         ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
         divs = ["Command", "Command", "Security", "Command", "Operations"]
         ids = ["100", "101", "102", "103", "104"]
@@ -13,62 +13,84 @@ def main():
         user_name = input("Please enter your full name: ")
 
         print(f"\n{user_name} logged in.")
-        print("___ Menu____")
-        print("1. Add Member")
-        print("2. Remove Member")
-        print("3. Update Rank")
-        print("4. Display Roster")
-        print("5. Search Crew")
-        print("6. Filter By Division")
-        print("7. Calculate Payroll")
-        print("8. Count Officers")
 
-        choice = input("Select choice: ")
+        while True:
+            print("___ Menu____")
+            print("1. Add Member")
+            print("2. Remove Member")
+            print("3. Update Rank")
+            print("4. Display Roster")
+            print("5. Search Crew")
+            print("6. Filter By Division")
+            print("7. Calculate Payroll")
+            print("8. Count Officers")
 
-        def add_member(names, ranks, divs, ids):
-            
+            choice = input("Select choice: ")
+        
+        
             if choice == "1":  
                 print("You choice is 1")
 
-                new_name = input("Name: ")
-                if new_name in names:
-                    print("Invalid name")
-                else:
-                    print("Name added")
-                    names.append(new_name)
+                def add_member(names, ranks, divs, ids):
+                    
+                    new_name = input("Name: ")
+                    if new_name in names:
+                        print("Invalid name")
+                    else:
+                        print("Name added")
+                        names.append(new_name)
                
-                new_rank = input("Rank: ")
-                if new_rank in ranks:
-                    print("Rank added")
-                    ranks.append(new_rank)
-                else:
-                    print("Invalid Rank")
+                    new_rank = input("Rank: ")
+                    if new_rank in ranks:
+                        print("Rank added")
+                        ranks.append(new_rank)
+                    else:
+                        print("Invalid Rank")
                     
-                new_div = input("Division: ")
-                if new_div in divs:
-                    print("Division added")
-                    divs.append(new_div)
-                else:
-                    print("Invalid Division")
+                    new_div = input("Division: ")
+                    if new_div in divs:
+                        print("Division added")
+                        divs.append(new_div)
+                    else:
+                        print("Invalid Division")
                     
-                new_ids = input("ID: ")
-                if new_ids in ids:
-                    print("Invalid ID")
-                else:
-                    print("ID is added")
-                    ids.append(new_ids)
+                    new_ids = input("ID: ")
+                    if new_ids in ids:
+                        print("Invalid ID")
+                    else:
+                        print("ID is added")
+                        ids.append(new_ids)
                 
+                add_member(names, ranks, divs, ids)
+        
+            elif choice == "2":
+                print("You choice is 2")    
+            
+                def remove_member(names, ranks, divs, ids):
+
+                    rem_id = input("Please enter the ID: ")
+
+                    if rem_id in ids:
+                        rem_ids = ids.index(rem_id)
+                        names.pop(rem_ids)
+                        ranks.pop(rem_ids)
+                        divs.pop(rem_ids)
+                        ids.pop(rem_ids)
+                        print("The member is removed")
+
+                    else:
+                        print("This crew member is not exist")
+                
+                remove_member(names, ranks, divs, ids)
 
 
             else:
                 print("Invalid number")
 
         
-        add_member(names, ranks, divs, ids)
+            
+            
 
     display_menu()
     
 main()
-
-
-    
